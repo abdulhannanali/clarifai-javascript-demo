5c9220f7d96dd9aa8b23811f5a2cad1917eb5aec
Hannan Ali <abdulhannanali@outlook.com>
2016-09-15 17:32:37 +0500

    clarifai application demo working

---
+++ js/keys.js
@@ -0,0 +1,12 @@
+// keys.js
+// Contains keys to run this demo
+// The keys should be kept secret and separate from the code
+// Enter your keys below to take the first in running this step 
+// VROOOOM VROOM
+
+function getKeys () {
+	return {
+		CLARIFAI_CLIENT_ID: "",
+		CLARIFAI_CLIENT_SECRET: ""
+	}
+}
